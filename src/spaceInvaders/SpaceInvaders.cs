32da1381b41ddd98dbb3675d2f5180c28ace25a6
Joshua Kitchen <57072030+kitchej@users.noreply.github.com>
2022-09-17 14:18:40 -0600

    added crossplatform support

--- src/spaceInvaders/SpaceInvaders.cs
+++ src/spaceInvaders/SpaceInvaders.cs
@@ -8,10 +8,10 @@ namespace spaceInvaders{
             Display screen = new Display(40, 60);
             Input input = new Input(exitKey: ConsoleKey.Q);
 
-            Sprite northBounds = new Sprite(@"src\spaceInvaders\sprites\horizBorder.txt", 1, 0, "northBounds");
-            Sprite southBounds = new Sprite(@"src\spaceInvaders\sprites\horizBorder.txt", 1, 39, "southBounds");
-            Sprite westBounds = new Sprite(@"src\spaceInvaders\sprites\vertiBorder.txt", 0, 1, "westBounds");
-            Sprite eastBounds = new Sprite(@"src\spaceInvaders\sprites\vertiBorder.txt", 59, 1, "eastBounds");
+            Sprite northBounds = new Sprite(Path.Join(new[] {"src", "spaceInvaders", "sprites", "horizBorder.txt"}), 1, 0, "northBounds");
+            Sprite southBounds = new Sprite(Path.Join(new[] {"src", "spaceInvaders", "sprites", "horizBorder.txt"}), 1, 39, "southBounds");
+            Sprite westBounds = new Sprite(Path.Join(new[] {"src", "spaceInvaders", "sprites", "vertiBorder.txt"}), 0, 1, "westBounds");
+            Sprite eastBounds = new Sprite(Path.Join(new[] {"src", "spaceInvaders", "sprites", "vertiBorder.txt"}), 59, 1, "eastBounds");
 
 
             List<Pawn> ships = new List<Pawn>();
@@ -21,7 +21,7 @@ namespace spaceInvaders{
             int starty = 1;
             for (int i=0;i<3;i++){
                 for(int j=0;j<6;j++){
-                    ships.Add(new Pawn(@"src\spaceInvaders\sprites\alien1.txt", startx: startx, starty: starty, spriteId: "enemyShip", display: screen));
+                    ships.Add(new Pawn(Path.Join(new[] {"src", "spaceInvaders", "sprites", "alien1.txt"}), startx: startx, starty: starty, spriteId: "enemyShip", display: screen));
                     startx += 5;
                 }
                 startx = 2;
@@ -30,15 +30,15 @@ namespace spaceInvaders{
             
             foreach (var s in ships){
                 screen.AddSprite(s);
-                controllers.Add(new EnemyController(s, screen, 500, new Spawner(typeof(Pawn), new object[] { @"src\spaceInvaders\sprites\projectile.txt", 30, 21, "enemyProjectile", screen, 0, true}, 
+                controllers.Add(new EnemyController(s, screen, 500, new Spawner(typeof(Pawn), new object[] { Path.Join(new[] {"src", "spaceInvaders", "sprites", "projectile.txt"}), 30, 21, "enemyProjectile", screen, 0, true}, 
                                           typeof(EnemyProjectileController), new object?[] {null, screen, 50}, 
                                           screen)));
             }
 
 
             
-            Pawn ship = new Pawn(@"src\spaceInvaders\sprites\ship.txt", startx: 30, starty: 36, spriteId: "ship", display: screen, stackOrder: 0);
-            Spawner shipProjectileSpawner = new Spawner(typeof(Pawn), new object[] { @"src\spaceInvaders\sprites\projectile.txt", 30, 21, "projectile", screen, 0, true}, 
+            Pawn ship = new Pawn(Path.Join(new[] {"src", "spaceInvaders", "sprites", "ship.txt"}), startx: 30, starty: 36, spriteId: "ship", display: screen, stackOrder: 0);
+            Spawner shipProjectileSpawner = new Spawner(typeof(Pawn), new object[] { Path.Join(new[] {"src", "spaceInvaders", "sprites", "projectile.txt"}), 30, 21, "projectile", screen, 0, true}, 
                                           typeof(ProjectileController), new object?[] {null, screen, 50}, 
                                           screen);
 
